fa0f005cfba24910aea775218f83b6c7f3b168c0
Björn <rocketscienc01100101@gmail.com>
2012-10-31 17:41:27 +0100

    test

---
+++ Javagotchi/src/Bunny.java
@@ -0,0 +1,10 @@
+
+public class Bunny {
+	
+	public void default() {
+		System.out.println("(\\___/)");
+		System.out.println("(='.'=)");
+		System.out.println("(\")_(\")");
+	}
+
+}
---
+++ Javagotchi/src/Cat.java
@@ -0,0 +1,11 @@
+
+public class Cat {
+	
+	public void sleep() {
+		System.out.println("        |\\      _,,,---,,_  ");
+		System.out.println("  PRrrr /,`.-'`'    -.  ;-;;,_ ");
+		System.out.println("       |,4-  ) )-,_. ,\\ (  `'-'");
+		System.out.println("      '---''(_/--'  `-'\\_)      ");
+	}
+	
+}
