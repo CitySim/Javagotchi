32e24a41e19c789c72eb9b22e987f102c5248715
Sven Tatter <sven31894@gmail.com>
2012-11-04 14:56:16 +0100

    version 1

--- Javagotchi/src/Javagotchi.java
+++ Javagotchi/src/Javagotchi.java
@@ -1,5 +1,5 @@
 public class Javagotchi {
-	public static final String VERSION = "0.0.0";
+	public static final String VERSION = "1.0.0";
 	public static boolean Exit = false;
 
 	public static void main(String[] args) {
